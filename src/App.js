eb868523f85080394e3f0822556e8be2fceff9aa
Anna <eviltwin007@yandex.ru>
2023-06-05 18:38:00 -0700

    Added github URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App </div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>;
+      <footer>
+        This project was coded by Anna Ro and is
+        <a href="https://github.com/Anna-R0/fahrenheit-weather" target="_blank">
+          {" "}
+          open-sourced on github
+        </a>
+      </footer>
+      ;
+    </div>
+  );
 }
