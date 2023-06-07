73edef8c40daa67ba588fdc28f20210ecccbc7f1
Anna <eviltwin007@yandex.ru>
2023-06-06 17:49:54 -0700

    Added 1 day forecast

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,7 @@ import "./Weather.css";
 import axios from "axios";
 import FormattedDate from "./FormattedDate";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
   const [city, setCity] = useState(props.defaultCity);
@@ -59,6 +60,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
