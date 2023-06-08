565ffa4bbad07b612705028faa35b3f2038a885c
Anna <eviltwin007@yandex.ru>
2023-06-07 20:39:39 -0700

    Added real data 6 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 
 import axios from "axios";
@@ -8,13 +8,31 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
   if (loaded) {
-    return <WeatherForecastDay data={forecast[0]} />;
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 6) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
+    );
   } else {
     let apiKey = "b82fba609f6a6d89452421e5d04b5255";
     let longitude = props.coordinates.lon;
